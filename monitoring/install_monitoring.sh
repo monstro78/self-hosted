7da14ecf7699d15720029e70a96e18fe098ce585
Giorgos <giorgoskarag@gmail.com>
2022-02-09 20:26:26 +0200

    Update install_monitoring.sh

--- monitoring/install_monitoring.sh
+++ monitoring/install_monitoring.sh
@@ -4,4 +4,12 @@
 sudo mkdir -p /portainer/Files/AppData/Config/prometheus/config
 sudo mkdir -p /portainer/Files/AppData/Config/prometheus/data
 sudo mkdir -p /portainer/Files/AppData/Config/grafana/data
-sudo wget -O /portainer/Files/AppData/Config/prometheus/config/prometheus.yml
+
+#Download config file
+sudo wget -O /portainer/Files/AppData/Config/prometheus/config/prometheus.yml https://raw.githubusercontent.com/monstro78/self-hosted/main/monitoring/prometheus.yml
+sudo touch /portainer/Files/AppData/Config/grafana/grafana.ini
+
+#Permissions
+sudo chown -R 472:472 /portainer/Files/AppData/Config/grafana/data
+
+echo "Done!"
