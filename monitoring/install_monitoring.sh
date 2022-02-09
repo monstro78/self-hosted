73bddb88c5f71dbcc56650e60d2d265312723cf2
Giorgos <giorgoskarag@gmail.com>
2022-02-09 20:52:28 +0200

    Update install_monitoring.sh

--- monitoring/install_monitoring.sh
+++ monitoring/install_monitoring.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#Dhmiourgia directories
-sudo mkdir -p /portainer/Files/AppData/Config/prometheus/config
-sudo mkdir -p /portainer/Files/AppData/Config/prometheus/data
-sudo mkdir -p /portainer/Files/AppData/Config/grafana/data
+echo "Dhmiourgia directories..."
+sudo mkdir -p /portainer/Files/AppData/Config/prometheus/config || error "Egine malakia!"
+sudo mkdir -p /portainer/Files/AppData/Config/prometheus/data || error "Egine malakia!"
+sudo mkdir -p /portainer/Files/AppData/Config/grafana/data || error "Egine malakia!"
 
-#Download config file
+echo "Download config file..."
 sudo wget -O /portainer/Files/AppData/Config/prometheus/config/prometheus.yml https://raw.githubusercontent.com/monstro78/self-hosted/main/monitoring/prometheus.yml
+echo "Dhmiourgia blank config file"
 sudo touch /portainer/Files/AppData/Config/grafana/grafana.ini
 
-#Permissions
+echo "Setting permissions..." 
 sudo chown -R 472:472 /portainer/Files/AppData/Config/grafana/data
 
-echo "Done! --> Deploy to stack"
+echo "Done! --> Deploy stack"
