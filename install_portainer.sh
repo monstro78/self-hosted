7dd060d57e46bcfdca271bc5bae4e638ed68441a
Giorgos <giorgoskarag@gmail.com>
2022-02-10 10:56:01 +0200

    Update install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -2,3 +2,4 @@
 
 sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 echo "Done!!"
+echo " App template --> https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/template/portainer-v2-arm64.json"
